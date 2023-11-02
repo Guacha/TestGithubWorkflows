e7d28568ed77b450ec547a222b21528aff0ee623
Julian Espitaleta <guachamalinga@gmail.com>
2023-11-02 11:32:18 -0500

    start project

---
+++ math.js
@@ -0,0 +1,30 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+}
+
+function exponentiate(a, b) {
+    return Math.pow(a, b);
+}
+
+module.exports = {
+    sum,
+    subtract,
+    multiply,
+    divide,
+    exponentiate
+};
